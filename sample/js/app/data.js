8c34f296ff90504a739fe58d0c8de6cf91956f02
Ola Ellnestam <ola@agical.se>
2014-01-01 18:43:04 +0100

    Now adding edges from data-file

--- sample/js/app/data.js
+++ sample/js/app/data.js
@@ -6,7 +6,18 @@ define([], function() {
 	'c' : [[0, 20], [4, 45], [8, 40], [8, 20], [4, 3]],
 	'd' : [[0, 10], [2, 25], [6, 40], [6, 20], [2, 3]],
 	'e' : [[0, 10], [0, 25], [0, 40]],
-	'f' : [[0, 10], [0, 30], [0, 30]]
-    };
+	'f' : [[0, 10], [0, 30], [0, 30]],
+
+	edges: [
+	    {"f":"../data/sub/under2/apa.txt","t":"","p":"../data/sub/under2","info":{"lines":[[0,0]]}},
+	    {"f":"../data/sub/under2","t":"","p":"../data/sub","info":{"lines":[[0,0]]}},
+	    {"f":"../data/sub/under/deep.txt","t":"adededelk\n","p":"../data/sub/under","info":{"lines":[[0,9],[0,0]]}},
+	    {"f":"../data/sub/under","t":"","p":"../data/sub","info":{"lines":[[0,0]]}},
+	    {"f":"../data/sub/hazze.txt","t":"Innehåll i Hazze\n","p":"../data/sub","info":{"lines":[[0,16],[0,0]]}},
+	    {"f":"../data/sub","t":"","p":"../data","info":{"lines":[[0,0]]}},
+	    {"f":"../data/","t":"","p":"..","info":{"lines":[[0,0]]}}
+	]
+
+    }
 
 });
