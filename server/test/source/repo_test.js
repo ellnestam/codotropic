572d58693b621555276d3c17d8afb871d08f49e8
Ola Ellnestam <ola@agical.se>
2014-01-02 21:53:00 +0100

    Rewrite

--- server/test/source/repo_test.js
+++ server/test/source/repo_test.js
@@ -8,31 +8,11 @@ exports['Repo'] = {
 	done();
     },
     'Structure from name': function(test) {
-	var readFile = repo.createRead('test/example.file');
-	readFile.then(
-	    function(data) {
-		// console.log(data);
-		test.done();
-	    },
-	    function(err) {
-		console.error(err);
-	    }
-	);
+	test.done();
 
-	test.equals("de", "de");
     },
 
     'Read Dir' : function(test) {
-	var readDir = repo.createProcessDir('.');
-	var res = readDir.then( repo.doIt() );
-
-	var result = q('');
-	
-	res.then(function(result) {
-	    result.then(function(res) {
-		console.log(res);
-	    });
-	});
 
 	test.done();
     }
