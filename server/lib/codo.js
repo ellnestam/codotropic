47a5ba21a35e32160bb8b4b496c26c38e3d02823
Ola Ellnestam <ola@agical.se>
2014-01-03 14:52:02 +0100

    promisify

--- server/lib/codo.js
+++ server/lib/codo.js
@@ -18,36 +18,40 @@ var codo = {
 	    var codoBinPath = args[1];
 	    var dest = args[3];
 	    var sample_source = p.resolve(p.dirname(codoBinPath), '../../sample');
-	    codo.setupDir(copy.ncp, sample_source, dest);
 
-	    var dirToRead = args[2];
-	    var read = repo.scan(dirToRead, textInfo);
-	    read.then(codo.c2(p.join(dest, '/js/app/data.js')), codo.error).done();
+	    var setup = codo.setupDir(copy.ncp, sample_source, dest);
+	    
+	    setup.then(function() {
+		var dirToRead = args[2];
+		var read = repo.scan(dirToRead, textInfo);
+		read.then(codo.collect(p.join(dest, '/js/app/data.js')), 
+			  codo.error)
+		    .done();
+	    });
 	}
     },
 
     setupDir : function(cp, source, destination) {
-	cp.limit = 16;
+	var defered = q.defer();
+
+	cp.limit = 16;	
 	cp(source, destination, function (err) {
 	    if (err) {
-		return console.error(err);
+		defered.reject(new Error(err));
+	    } else {
+		defered.resolve('Source copied');
 	    }
-	    console.log('Source copied');
 	});
+
+	return defered.promise;
     },
 
-    c2 : function(fileName) {
+    collect : function(fileName) {
 	return function(d) {
 	    var promises = [].concat.apply([], d);
 	    var p2 = [].concat.apply([], promises);
 	    writer.createDataFile(d, fileName);
 	};
-    },
-
-    collect : function(d) {
-	var promises = [].concat.apply([], d);
-	var p2 = [].concat.apply([], promises);
-	writer.createDataFile(d);
     },
 
     error : function error(e) {
